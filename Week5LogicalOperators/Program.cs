2b43f57bcaf953214bb4c8bdc0b808b7f297592d
Liis VT <liis.viidas@tktk.ee>
2023-03-30 19:59:00 +0300

    create new app

---
+++ Week5LogicalOperators/Program.cs
@@ -0,0 +1,15 @@
+﻿// AND - &&
+//koik tingimused peavad olema taidetud
+// true && true && true --> true
+
+//true && false --> false
+//false && true --> false
+//false && false --> false
+
+//OR - || (pipes) 
+//vahemalt yks tingimus peab olema taidetud
+// true || true --> true
+// true || false --> true
+// false || true --> true
+// false || false --> false
+
